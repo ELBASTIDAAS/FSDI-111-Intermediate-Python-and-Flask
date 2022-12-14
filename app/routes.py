1528a6f23a5360bb71b3061b8b3508f081ebb911
ELBASTIDAAS <miguel.bastidas18@tectijuana.edu.mx>
2022-12-13 20:05:53 -0800

    class 2

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 # Import Flask to allow us to create our app.
 from flask import Flask
+from flask import render_template
+
+from datetime import datetime
 
 # Global variable __name__ tells Flask whether or not we are running the file
 app = Flask(__name__)
@@ -9,10 +12,15 @@ app = Flask(__name__)
 @app.get("/")
 def index():                        # function to the '/' route. This means that whenever we send a request to
     out = {
-        "fName": "Miguel",
-        "lName": "Bastidas",
-        "age": 24,
-        "hobbies": ["Coding", "Gaming", "Reading"],
+        "first_Name": "Miguel",
+        "last_Name": "Bastidas",
+        "hobbies": "Coding, Reading, and Playing Video Games",
         "is_active": True
     }
     return out                      # Return the string 'out' as a response.
+
+
+@app.get("/about")
+def about():
+    time_stamp = datetime.now().strftime("%F %H:%M:%S")
+    return render_template("about.html", timestamp=time_stamp)
